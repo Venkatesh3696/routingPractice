9fe985cacbc35fcc79fae2885a87598e31c7b6d0
venkatesh <venkateshpentakota888@gmail.com>
2023-08-18 16:09:30 +0000

    first commit

---
+++ src/components/About/index.js
@@ -0,0 +1,12 @@
+// Write your JS code here
+const About = () => (
+  <div>
+    <img
+      alt="about"
+      src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+    />
+    <h1>About</h1>
+  </div>
+)
+
+export default About
